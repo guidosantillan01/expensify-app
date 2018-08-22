deacb5c9445f0dcebe0e16f30701faa06e77b01f
Guido Santillan <guidosantillan01@gmail.com>
2018-08-22 15:32:16 -0500

    Store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -7,7 +7,8 @@ export const addExpense = (expense) => ({
 })
 
 export const startAddExpense = (expenseData = {}) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid
     const {
       description = '', 
       note = '', 
@@ -16,7 +17,7 @@ export const startAddExpense = (expenseData = {}) => {
     } = expenseData
     const expense = { description, note, amount, createdAt }
 
-    return database.ref('expenses')
+    return database.ref(`users/${uid}/expenses`)
       .push(expense)
       .then((ref) => {
         dispatch(addExpense({
@@ -34,8 +35,10 @@ export const removeExpense = ({ id } = {}) => ({
 })
 
 export const startRemoveExpense = ({ id } = {}) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).remove()
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid
+
+    return database.ref(`users/${uid}/expenses/${id}`).remove()
       .then(() => {
         dispatch(removeExpense({ id }))
       })
@@ -50,8 +53,10 @@ export const editExpense = (id, updates) => ({
 })
 
 export const startEditExpense = (id, updates) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).update(updates)
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid
+
+    return database.ref(`users/${uid}/expenses/${id}`).update(updates)
       .then(() => {
         dispatch(editExpense(id, updates))
       })
@@ -65,8 +70,10 @@ export const setExpenses = (expenses) => ({
 })
 
 export const startSetExpenses = () => {
-  return (dispatch) => {
-    return database.ref('expenses')
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid
+
+    return database.ref(`users/${uid}/expenses`)
       .once('value')
       .then((snapshot) => {
         const expenses = []
