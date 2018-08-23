393d274307ee5609e0107e530dc73d02f13dcda9
Guido Santillan <guidosantillan01@gmail.com>
2018-08-23 14:56:58 -0500

    Add Expensify styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -61,14 +61,14 @@ export default class ExpenseForm extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p> }
-        <form onSubmit={this.onSubmit}>
+    return (       
+        <form className="form" onSubmit={this.onSubmit}>
+          {this.state.error && <p className="form__error">{this.state.error}</p> }
           <input 
             type="text"
             placeholder="Description"
             autoFocus
+            className="text-input"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
@@ -76,6 +76,7 @@ export default class ExpenseForm extends React.Component {
           <input 
             type="text"
             placeholder="Amount"
+            className="text-input"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
@@ -91,14 +92,16 @@ export default class ExpenseForm extends React.Component {
 
           <textarea
             placeholder="Add a note for your expense"
+            className="textarea"
             value={this.state.note}
             onChange={this.onNoteChange}
           >
           </textarea>
 
-          <button>Add Expense</button>
+          <div>
+            <button className="button">Save Expense</button>
+          </div>
         </form>
-      </div>
     )
   }
 }
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -6,18 +6,21 @@ import { connect } from 'react-redux'
 import { removeExpense } from '../actions/expenses'
 
 export const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
-  <div>
-    <Link to={`/edit/${id}`}>
-      <h3>{description}</h3>
-    </Link>
-    <p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
-    <p>Created at: {moment(createdAt).format('MMMM Do, YYYY')}</p>
-    <button
-      onClick={() => {
-        dispatch(removeExpense({ id }))
-      }}
-    >Remove</button>
-  </div> 
+  <Link to={`/edit/${id}`} className="list-item">
+    <div>
+      <h3 className="list-item__title">{description}</h3>
+      <span className="list-item__subtitle">{moment(createdAt).format('MMMM Do, YYYY')}</span>
+    </div>
+    <h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
+    <div>
+      <button
+        onClick={() => {
+          dispatch(removeExpense({ id }))
+        }}
+        className="button button--secondary"
+      >x</button>
+    </div>
+  </Link>   
 )
 
 export default connect()(ExpenseListItem)
